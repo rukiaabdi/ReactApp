28634cc106146791bae702fedad21f5db78ef549
rukiaabdi <ruuneeyqali11@gmail.com>
2026-09-23 06:39:01 -0700

    submited exercise12

---
+++ src/exercise12/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./exercise12/Counter";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/exercise12/Counter.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+
+      <button onClick={handleIncrement}>
+        Increment
+      </button>
+
+      <button onClick={handleDecrement}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
